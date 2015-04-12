03557ec2a2d05918cb3fd75e22af99dbfe543cec
Vicens Fayos <datracka@gmail.com>
2015-04-12 18:28:57 +0200

    small style improvements plus new header

--- wp-content/themes/syntax-child/header.php
+++ wp-content/themes/syntax-child/header.php
@@ -36,99 +36,53 @@ $path     = get_theme_mod( 'jetpack-path' );
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<?php if ( get_header_image() ) : ?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-		</a>
-		<?php endif; // End header image check. ?>
-		<div class="site-header-wrapper">
-
-			<div class="site-branding" style="display:inline">
-						<a class="site-logo-link-dl" href="http://datalook.io" style="vertical-align:middle">
-							 <svg class="logo-dl" width="161" height="24">
-									<image xlink:href="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/syntax-child/images/logo-datalook.svg" src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/syntax/images/logo.jpg" width="161" height="24"/>
-							</svg>
-						</a>
-					  <h1 class="site-title-dl">
-							<?php bloginfo( 'name' ); ?>
-						</h1>
+	<header id="masthead" class="header" role="banner">
+			<div class="nav-module header-logo">
+				<h1 class="logo logo-image">
+					<a href="/">
+						<img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/syntax-child/images/logo-datalook.svg" alt="DataLook">
+					</a>
+				</h1>
 			</div>
-			<?php if ( $facebook || $twitter || $tumblr || $linkedin || $google || $path || true) : ?>
-				<div class="social-links" style="display:inline">
-				<?php if ( $facebook ) : ?>
-					<a href="<?php echo esc_url( $facebook ); ?>" class="facebook-link">
-						<span class="screen-reader-text"><?php esc_html_e( 'Facebook', 'syntax' ); ?></span>
-					</a>
-				<?php endif; ?>
-				<?php if ( $google ) : ?>
-					<a href="<?php echo esc_url( $google ); ?>" class="google-link">
-						<span class="screen-reader-text"><?php esc_html_e( 'Google+', 'syntax' ); ?></span>
-					</a>
-				<?php endif; ?>
-				<?php if ( $linkedin ) : ?>
-					<a href="<?php echo esc_url( $linkedin ); ?>" class="linkedin-link">
-						<span class="screen-reader-text"><?php esc_html_e( 'LinkedIn', 'syntax' ); ?></span>
-					</a>
-				<?php endif; ?>
-				<?php if ( $path ) : ?>
-					<a href="<?php echo esc_url( $path ); ?>" class="path-link">
-						<span class="screen-reader-text"><?php esc_html_e( 'Path', 'syntax' ); ?></span>
-					</a>
-				<?php endif; ?>
-				<?php if ( $twitter || true) : ?>
-
-       <a href="https://twitter.com/opendataforgood" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @OpenDataForGood</a>
-       <span class="twitter-script"><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></span>
-				<?php endif; ?>
-				<?php if ( $tumblr) : ?>
-					<a href="<?php echo esc_url( $tumblr ); ?>" class="tumblr-link" >
-						<span class="screen-reader-text"><?php esc_html_e( 'Tumblr', 'syntax' ); ?></span>
-					</a>
-				<?php endif; ?>
-					</div>
-			<?php endif; ?>
-
-
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-		</div>
-		<hr style="clear:both; margin 0 auto; background-color: #444444; height:1px; margin-bottom: 0px"/>
-
-		<!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
-
-<div id="mc_embed_signup">
-	<div class="content-area">
-		<form action="//datalook.us9.list-manage.com/subscribe/post?u=4aa126af9096681d9931557cf&amp;id=dabaf9c94f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    		<div id="mc_embed_signup_scroll">
-				<div class="newsletter-dl mc-field-group" style="">
-	  				<div class="row">
-	      				<div class="col-md-5">
-	        				<label for="mce-EMAIL" style="display:inline-block; text-align:center; width:100%; line-height:32px; ">Get news about Data for Good</label>
-	      				</div>
-	      				<div class="col-md-5">
-									<input type="email" value="" name="EMAIL" placeholder="Your email" class="required email" id="mce-EMAIL" style="height:32px; padding:0px; border-radius:0px; ">
-	      				</div>
-	      				<div class="col-md-2">
-									<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button email-button email-button-dl" style="background-color:#8cc2d9; padding:0px; height:32px; border-radius:0px;">
-	      				</div>
-	  				</div>
-				</div>
-				<div id="mce-responses" class="clear">
-					<div class="response" id="mce-error-response" style="display:none">Hmm, an error occured...</div>
-					<div class="response" id="mce-success-response" style="display:none">Thanks for subscribing!</div>
-				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-	    		<div style="position: absolute; left: -5000px;">
-	    			<input type="text" name="b_4aa126af9096681d9931557cf_dabaf9c94f" tabindex="-1" value="">
-	    		</div>
-	    	</div>
-		</form>
-	</div>
-	<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-</div>
-<!--End mc_embed_signup-->
-
+		  <div class="nav-module">
+			</div>
+		  <div class="nav-module header-menu">
+				<strong>Blog</strong> | <a href="http://datalook.io" target="_blank">Home</a>
+			</div>
 	</header><!-- #masthead -->
+	<!-- Begin MailChimp Signup Form -->
+	<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+
+	<div id="mc_embed_signup" class="mc_embed_signup">
+		<div class="content-area">
+			<form action="//datalook.us9.list-manage.com/subscribe/post?u=4aa126af9096681d9931557cf&amp;id=dabaf9c94f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<div id="mc_embed_signup_scroll">
+					<div class="newsletter-dl mc-field-group" style="">
+						<div class="row">
+							<div class="col-md-5">
+								<label for="mce-EMAIL" style="display:inline-block; text-align:center; width:100%; line-height:32px; ">Get news about Data for Good</label>
+							</div>
+							<div class="col-md-5">
+								<input type="email" value="" name="EMAIL" placeholder="Your email" class="required email" id="mce-EMAIL" style="height:32px; padding:0px; border-radius:0px; ">
+							</div>
+							<div class="col-md-2">
+								<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button email-button email-button-dl" style="background-color:#8cc2d9; padding:0px; height:32px; border-radius:0px;">
+							</div>
+						</div>
+					</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none">Hmm, an error occured...</div>
+						<div class="response" id="mce-success-response" style="display:none">Thanks for subscribing!</div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					<div style="position: absolute; left: -5000px;">
+						<input type="text" name="b_4aa126af9096681d9931557cf_dabaf9c94f" tabindex="-1" value="">
+					</div>
+				</div>
+			</form>
+		</div>
+		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+	</div>
+	<!--End mc_embed_signup-->
 	<div id="main" class="site-main">
 		<h1 id="toggle-nav" class="menu-toggle"><span class="screen-reader-text"><?php _e( 'Menu', 'syntax' ); ?></span></h1>
 		<nav id="site-navigation" class="navigation-main" role="navigation">
