0546f5671828c35b13dec171445352b3e17bf907
Vicens Fayos <datracka@gmail.com>
2015-05-10 14:55:30 +0200

    added piwiki analytics account

--- wp-content/themes/syntax-child/footer.php
+++ wp-content/themes/syntax-child/footer.php
@@ -24,17 +24,20 @@ Theme: Syntax by Automattic.<?php
 <!-- Piwik -->
 <script type="text/javascript">
   var _paq = _paq || [];
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setDomains", ["*.blog.datalook.io"]]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
     var u="//piwik.vicensfayos.com/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 2]);
+    _paq.push(['setSiteId', 3]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="//piwik.vicensfayos.com/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="//piwik.vicensfayos.com/piwik.php?idsite=3" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
+
 </body>
 </html>
